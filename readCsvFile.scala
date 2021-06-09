4aeb8144b38df931b3ae5874474e6b5d4a27b23b
Faima02 <46047182+Faima02@users.noreply.github.com>
2021-06-09 21:24:18 +0200

    Update readCsvFile.scala

--- readCsvFile.scala
+++ readCsvFile.scala
@@ -1,5 +1,5 @@
  val df = spark.read
          .format("csv")
          .option("header", "true") //first line in file has headers
-         .option("mode", "DROPMALFORMED")
-         .load("hdfs:///csv/file/dir/file.csv")
+         .option("delimiter", "\t") //set delimiter to tab or comma
+         .load("c/user/home/file1.csv")
